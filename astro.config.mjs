882ec628cc8ce0a6dc1b7be2db379bd34e01355f
oemer.aran95@gmail.com <oemer.aran95@gmail.com>
2024-09-17 13:56:17 +0200

    feat(infra): use netlify adapter for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,19 @@
 import { defineConfig } from "astro/config"
 import react from "@astrojs/react"
 import tailwind from "@astrojs/tailwind"
-import node from "@astrojs/node"
+import sitemap from "@astrojs/sitemap"
 
-import sitemap from "@astrojs/sitemap";
+import netlify from "@astrojs/netlify"
 
 // https://astro.build/config
 export default defineConfig({
   output: "hybrid",
-  adapter: node({
-    mode: "standalone",
-  }),
-  integrations: [react(), tailwind({ applyBaseStyles: false }), sitemap()],
+  integrations: [
+    react(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    sitemap(),
+  ],
+  adapter: netlify(),
 })
